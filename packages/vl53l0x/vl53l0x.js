29fed5f3cf4dd3c2f3c5a02c2da81d93c3f79a53
Kohei Watanabe <kou029w@gmail.com>
2019-11-16 17:56:29 +0900

    yarn build

--- packages/vl53l0x/vl53l0x.js
+++ packages/vl53l0x/vl53l0x.js
@@ -256,7 +256,7 @@
           // or (reference_spad_count) bits have already been enabled, so
           // zero this bit.
           ref_spad_map[1 + Math.floor(i / 8)] &= ~(1 << i % 8);
-        } else if ((ref_spad_map[1 + Math.floor(i / 8)] >> i % 8) & (0x1 > 0)) {
+        } else if (((ref_spad_map[1 + Math.floor(i / 8)] >> i % 8) & 0x01) > 0) {
           spads_enabled += 1;
         }
       }
@@ -451,11 +451,11 @@
       // based on VL53L0X_GetSequenceStepEnables() from ST API
       var dc = this.devConst;
       var sequence_config = await this.i2cSlave.read8(dc._SYSTEM_SEQUENCE_CONFIG);
-      var tcc = (sequence_config >> 4) & (0x1 > 0);
-      var dss = (sequence_config >> 3) & (0x1 > 0);
-      var msrc = (sequence_config >> 2) & (0x1 > 0);
-      var pre_range = (sequence_config >> 6) & (0x1 > 0);
-      var final_range = (sequence_config >> 7) & (0x1 > 0);
+      var tcc = (sequence_config >> 4) & 0x01;
+      var dss = (sequence_config >> 3) & 0x01;
+      var msrc = (sequence_config >> 2) & 0x01;
+      var pre_range = (sequence_config >> 6) & 0x01;
+      var final_range = (sequence_config >> 7) & 0x01;
       return {
         tcc: tcc,
         dss: dss,
@@ -523,7 +523,7 @@
       timeout_period_mclks,
       vcsel_period_pclks
     ) {
-      macro_period_ns = Math.floor(
+      var macro_period_ns = Math.floor(
         (2304 * vcsel_period_pclks * 1655 + 500) / 1000
       );
       return Math.floor(
@@ -789,7 +789,7 @@
           period_pclks
         );
         if (enables & dc._SEQUENCE_ENABLE_PRE_RANGE) {
-          new_final_range_timeout_mclks += timeouts.pre_range_mclks;
+          new_final_range_timeout_mclks += st.pre_range_mclks;
         }
         await this._write_u16(
           dc._FINAL_RANGE_CONFIG_TIMEOUT_MACROP_HI,
