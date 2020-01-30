767668a18f2f861427ab35d234e5e586c3b8abfc
Masato Tomita <tomita13112@gmail.com>
2020-01-30 16:40:50 +0900

    Change "var" (variables definition) to "const"

--- node-examples/adt7410/main.js
+++ node-examples/adt7410/main.js
@@ -6,13 +6,13 @@ const sleep = promisify(setTimeout)
 main();
 
 async function main() {
-  var i2cAccess = await requestI2CAccess(); // i2cAccessを非同期で取得
-  var port = i2cAccess.ports.get(1); // I2C I/Fの1番ポートを取得
-  var adt7410 = new ADT7410(port, 0x48); // 取得したポートの0x48アドレスをADT7410ドライバで受信する
+  const i2cAccess = await requestI2CAccess(); // i2cAccessを非同期で取得
+  const port = i2cAccess.ports.get(1); // I2C I/Fの1番ポートを取得
+  const adt7410 = new ADT7410(port, 0x48); // 取得したポートの0x48アドレスをADT7410ドライバで受信する
   await adt7410.init();
   for (;;) {
     // 無限ループ
-    var value = await adt7410.read();
+    const value = await adt7410.read();
     console.log(value ? `${value} degree` : "Measurement failure");
     await sleep(1000);
   }
