83b0c0619bbd918a39c46ee7be546fedd2fd60e6
Masato Tomita <tomita13112@gmail.com>
2020-01-30 14:55:52 +0900

    make drivers read as module create node.js example.

--- node-examples/adt7410/main.js
+++ node-examples/adt7410/main.js
@@ -1,18 +1,19 @@
-import ADT7410 from "https://unpkg.com/@chirimen/adt7410?module";
-
+const { requestI2CAccess } = require("node-web-i2c");
+const ADT7410 = require("@chirimen/adt7410");
+const { promisify } = require("util");
+const sleep = promisify(setTimeout)
 
 main();
 
 async function main() {
-  var head = document.getElementById("head");
-  var i2cAccess = await navigator.requestI2CAccess(); // i2cAccessを非同期で取得
+  var i2cAccess = await requestI2CAccess(); // i2cAccessを非同期で取得
   var port = i2cAccess.ports.get(1); // I2C I/Fの1番ポートを取得
   var adt7410 = new ADT7410(port, 0x48); // 取得したポートの0x48アドレスをADT7410ドライバで受信する
   await adt7410.init();
   for (;;) {
     // 無限ループ
     var value = await adt7410.read();
-    head.innerHTML = value ? `${value} degree` : "Measurement failure";
+    console.log(value ? `${value} degree` : "Measurement failure");
     await sleep(1000);
   }
 }
---
+++ node-examples/adt7410/readme.md
@@ -0,0 +1,4 @@
+# 回路図
+
+![回路図1](./schematic.png "schematic")
+![回路図2](./schematic_warning.png "schematic")
