b261f0a17b3c5b8916dc31e75d79b79a40e3f998
Masato Tomita <tomita13112@gmail.com>
2020-02-04 11:34:52 +0900

    Add an example for Node.js #8 #22

---
+++ node-examples/ssd1308/main.js
@@ -0,0 +1,22 @@
+const { requestI2CAccess } = require("node-web-i2c");
+const OledDisplay = require("@chirimen/grove-oled-display");
+const { promisify } = require("util");
+const sleep = promisify(setTimeout);
+
+main();
+
+async function main() {
+  const i2cAccess = await requestI2CAccess();
+  console.log("initializing...");
+  const port = i2cAccess.ports.get(1);
+  const display = new OledDisplay(port);
+  await display.init();
+  display.clearDisplayQ();
+  await display.playSequence();
+  console.log("drawing text...");
+  display.drawStringQ(0, 0, "hello");
+  display.drawStringQ(1, 0, "Real");
+  display.drawStringQ(2, 0, "World");
+  await display.playSequence();
+  console.log("completed");
+}
