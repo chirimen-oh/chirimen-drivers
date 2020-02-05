7f46fdbf3e719bb691359eb8a95289ad002c0906
Kohei Watanabe <kou029w@gmail.com>
2020-02-05 12:16:48 +0900

    コメント位置をnode-examplesに合わせる

--- examples/pca9685/main.js
+++ examples/pca9685/main.js
@@ -4,13 +4,12 @@ main();
 
 async function main() {
   const head = document.getElementById("head");
-  
-  // servo setting for sg90
-  // Servo PWM pulse: min=0.0011[sec], max=0.0019[sec] angle=+-60[deg]
   const i2cAccess = await navigator.requestI2CAccess();
   const port = i2cAccess.ports.get(1);
   const pca9685 = new PCA9685(port, 0x40);
   let angle = 0;
+  // servo setting for sg90
+  // Servo PWM pulse: min=0.0011[sec], max=0.0019[sec] angle=+-60[deg]
   await pca9685.init(0.001, 0.002, 30);
   for (;;) {
     angle = angle <= -30 ? 30 : -30;
