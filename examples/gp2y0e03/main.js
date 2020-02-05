42b90056bc12a8f60baa2c54004368468010b1dc
Masato Tomita <tomita13112@gmail.com>
2020-02-05 11:00:31 +0900

    Remove needless expection handling.

--- examples/gp2y0e03/main.js
+++ examples/gp2y0e03/main.js
@@ -4,26 +4,23 @@ main();
 
 async function main() {
   const valelem = document.getElementById("distance");
-  try {
-    const i2cAccess = await navigator.requestI2CAccess();
-    const port = i2cAccess.ports.get(1);
-    const sensor_unit = new GP2Y0E03(port, 0x40);
-    await sensor_unit.init();
 
-    while (1) {
-      try {
-        const distance = await sensor_unit.read();
-        if (distance != null) {
-          valelem.innerHTML = "Distance:" + distance + "cm";
-        } else {
-          valelem.innerHTML = "out of range";
-        }
-      } catch (err) {
-        console.error("READ ERROR:" + err);
+  const i2cAccess = await navigator.requestI2CAccess();
+  const port = i2cAccess.ports.get(1);
+  const sensorUnit = new GP2Y0E03(port, 0x40);
+  await sensorUnit.init();
+
+  while (true) {
+    try {
+      const distance = await sensorUnit.read();
+      if (distance != null) {
+        valelem.innerHTML = "Distance:" + distance + "cm";
+      } else {
+        valelem.innerHTML = "out of range";
       }
-      await sleep(500);
+    } catch (err) {
+      console.error("READ ERROR:" + err);
     }
-  } catch (err) {
-    console.error("GP2Y0E03 init error");
+    await sleep(500);
   }
 }
