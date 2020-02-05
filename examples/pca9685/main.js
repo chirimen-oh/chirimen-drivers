36c5fe86c49a67656205a5e708e89cc4d41f332f
Kohei Watanabe <kou029w@gmail.com>
2020-02-05 12:24:27 +0900

    変数を用いずやさしく書き直す

--- examples/pca9685/main.js
+++ examples/pca9685/main.js
@@ -7,14 +7,15 @@ async function main() {
   const i2cAccess = await navigator.requestI2CAccess();
   const port = i2cAccess.ports.get(1);
   const pca9685 = new PCA9685(port, 0x40);
-  let angle = 0;
   // servo setting for sg90
   // Servo PWM pulse: min=0.0011[sec], max=0.0019[sec] angle=+-60[deg]
   await pca9685.init(0.001, 0.002, 30);
   for (;;) {
-    angle = angle <= -30 ? 30 : -30;
-    await pca9685.setServo(0, angle);
-    head.innerHTML = angle;
+    await pca9685.setServo(0, 30);
+    head.textContent = "30 deg";
+    await sleep(1000);
+    await pca9685.setServo(0, -30);
+    head.textContent = "-30 deg";
     await sleep(1000);
   }
 }
--- node-examples/pca9685/main.js
+++ node-examples/pca9685/main.js
@@ -9,14 +9,15 @@ async function main() {
   const i2cAccess = await requestI2CAccess();
   const port = i2cAccess.ports.get(1);
   const pca9685 = new PCA9685(port, 0x40);
-  let angle = 0;
   // servo setting for sg90
   // Servo PWM pulse: min=0.0011[sec], max=0.0019[sec] angle=+-60[deg]
   await pca9685.init(0.001, 0.002, 30);
   for (;;) {
-    angle = angle <= -30 ? 30 : -30;
-    await pca9685.setServo(0, angle);
-    console.log(angle);
+    await pca9685.setServo(0, -30);
+    console.log("-30 deg");
+    await sleep(1000);
+    await pca9685.setServo(0, 30);
+    console.log("30 deg");
     await sleep(1000);
   }
 }
