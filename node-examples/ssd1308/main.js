c1d9fc034c9069c8b68bfe6c2c404c0e19fd5404
Masato Tomita <tomita13112@gmail.com>
2020-02-06 17:14:25 +0900

    Remove the non used code.

--- node-examples/ssd1308/main.js
+++ node-examples/ssd1308/main.js
@@ -1,7 +1,5 @@
 const { requestI2CAccess } = require("node-web-i2c");
 const OledDisplay = require("@chirimen/grove-oled-display");
-// const { promisify } = require("util");
-// const sleep = promisify(setTimeout);
 
 main();
 
@@ -14,7 +12,7 @@ async function main() {
   display.clearDisplayQ();
   await display.playSequence();
   console.log("drawing text...");
-  display.drawStringQ(0, 0, "hello");
+  display.drawStringQ(0, 0, "Hello");
   display.drawStringQ(1, 0, "Real");
   display.drawStringQ(2, 0, "World");
   await display.playSequence();
