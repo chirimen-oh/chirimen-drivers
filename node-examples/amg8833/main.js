1aceaa653da9dffe3259bef53fc077c5434cf8a9
Masato Tomita <tomita13112@gmail.com>
2020-02-06 13:50:40 +0900

    Create a Node.js example.

---
+++ node-examples/amg8833/main.js
@@ -0,0 +1,26 @@
+const { requestI2CAccess } = require("node-web-i2c");
+const AMG8833 = require("@chirimen/amg8833");
+const { promisify } = require("util");
+const sleep = promisify(setTimeout);
+
+main();
+
+async function main() {
+  const i2cAccess = await requestI2CAccess();
+  const port = i2cAccess.ports.get(1);
+  const amg8833 = new AMG8833(port, 0x69);
+  await amg8833.init();
+
+  while (true) {
+    const data = await amg8833.readData();
+    let output = "";
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].length; j++) {
+        data[i][j] = data[i][j].toFixed(2);
+      }
+      console.log(data[i].join(" "));
+    }
+    console.log();
+    sleep(500);
+  }
+}
---
+++ node-examples/amg8833/readme.md
@@ -0,0 +1,5 @@
+# AMG8833 サーモグラフィー
+
+## 配線図
+
+![配線図](./schematic.png "schematic")
