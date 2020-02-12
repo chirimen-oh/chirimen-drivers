63edd477bfca22aaecbb00760fee6cc899f21dea
Masato Tomita <tomita13112@gmail.com>
2020-02-12 13:52:59 +0900

    Fix typo.

--- examples/ads1x15/main.js
+++ examples/ads1x15/main.js
@@ -7,12 +7,12 @@ async function main() {
   const i2cAccess = await navigator.requestI2CAccess();
   const port = i2cAccess.ports.get(1);
   const ads1x15 = new ADS1X15(port, 0x48);
-  // If you uses ADS1X15, you have to select "true", otherwise select "false".
+  // If you uses ADS1115, you have to select "true", otherwise select "false".
   await ads1x15.init(true);
   console.log("init complete");
   while (true) {
     let output = "";
-    // ADS1X15 has 4 channels.
+    // ADS1115 has 4 channels.
     for (let channel = 0; channel < 4; channel++) {
       const rawData = await ads1x15.read(channel);
       const voltage = ads1x15.getVoltage(rawData);
