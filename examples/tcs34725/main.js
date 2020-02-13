543ef47107b7fe041a20f830c0b1ab0c6f07150d
Masato Tomita <tomita13112@gmail.com>
2020-02-13 12:03:17 +0900

    Make the browser example

---
+++ examples/tcs34725/main.js
@@ -0,0 +1,24 @@
+import TCS34725 from "https://unpkg.com/@chirimen/tcs34725?module";
+
+main();
+
+async function main() {
+  const rgbDisplay = document.getElementById("rgbDisplay");
+  const i2cAccess = await navigator.requestI2CAccess();
+  const port = i2cAccess.ports.get(1);
+  const tcs34725 = new TCS34725(port, 0x29);
+  await tcs34725.init();
+  await tcs34725.gain(4);
+
+  while (true) {
+    const data = await tcs34725.read();
+    rgbDisplay.innerHTML = [
+      `R: ${data.r}`,
+      `G: ${data.g}`,
+      `B: ${data.b}`,
+      `C: ${data.c}`
+    ].join("<br>");
+
+    await sleep(500);
+  }
+}
