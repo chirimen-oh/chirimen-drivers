61c37737fae7b724512def83fa70f3868e982bfa
Masato Tomita <tomita13112@gmail.com>
2020-02-13 12:03:29 +0900

    Make the Node.js example

---
+++ node-examples/tcs34725/main.js
@@ -0,0 +1,25 @@
+const { requestI2CAccess } = require("node-web-i2c");
+const TCS34725 = require("@chirimen/tcs34725");
+const { promisify } = require("util");
+const sleep = promisify(setTimeout);
+
+main();
+
+async function main() {
+  const i2cAccess = await requestI2CAccess();
+  const port = i2cAccess.ports.get(1);
+  const tcs34725 = new TCS34725(port, 0x29);
+  await tcs34725.init();
+  await tcs34725.gain(4);
+
+  while (true) {
+    const data = await tcs34725.read();
+    console.log(
+      [`R: ${data.r}`, `G: ${data.g}`, `B: ${data.b}`, `C: ${data.c}`].join(
+        ", "
+      )
+    );
+
+    await sleep(500);
+  }
+}
