9532a45bf72a5c40fd3e3f357fe7761662362715
Masato Tomita <tomita13112@gmail.com>
2020-02-19 13:38:08 +0900

    Change the format of the numbers from hex to int

--- node-examples/neopixel-i2c/main.js
+++ node-examples/neopixel-i2c/main.js
@@ -10,14 +10,13 @@ async function main() {
   const port = i2cAccess.ports.get(1);
   const neopixel_i2c = new NEOPIXEL_I2C(port, 0x41);
   await neopixel_i2c.init();
-  console.log("initialization complete");
 
   while (true) {
-    await neopixel_i2c.setGlobal(0x20, 0x00, 0x00); // Red
+    await neopixel_i2c.setGlobal(64, 0, 0); // Red
     await sleep(200);
-    await neopixel_i2c.setGlobal(0x00, 0x20, 0x00); // Green
+    await neopixel_i2c.setGlobal(0, 64, 0); // Green
     await sleep(200);
-    await neopixel_i2c.setGlobal(0x00, 0x00, 0x20); // Blue
+    await neopixel_i2c.setGlobal(0, 0, 64); // Blue
     await sleep(200);
   }
 }
