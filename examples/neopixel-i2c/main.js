f704fac92eeae00e78e9ba13f592e5bab19eb935
Masato Tomita <tomita13112@gmail.com>
2020-02-19 14:01:16 +0900

    Fix codes to camel case

--- examples/neopixel-i2c/main.js
+++ examples/neopixel-i2c/main.js
@@ -1,4 +1,4 @@
-import NEOPIXEL_I2C from "https://unpkg.com/@chirimen/neopixel-i2c?module";
+import NEOPIXEL from "https://unpkg.com/@chirimen/neopixel-i2c?module";
 
 main();
 
@@ -7,10 +7,10 @@ async function main() {
   const color = document.getElementById("colorPicker");
   const i2cAccess = await navigator.requestI2CAccess();
   const port = i2cAccess.ports.get(1);
-  const neopixel_i2c = new NEOPIXEL_I2C(port, 0x41);
-  await neopixel_i2c.init();
+  const neopixel = new NEOPIXEL(port, 0x41);
+  await neopixel.init();
   initDisplay.remove();
-  await neopixel_i2c.setGlobal(0x10, 0x10, 0x10);
+  await neopixel.setGlobal(0x10, 0x10, 0x10);
 
   color.set.addEventListener("click", async () => {
     console.log([
@@ -18,7 +18,7 @@ async function main() {
       color.green.value,
       color.blue.value
     ]);
-    await neopixel_i2c.setGlobal(
+    await neopixel.setGlobal(
       parseInt(color.red.value, 10),
       parseInt(color.green.value, 10),
       parseInt(color.blue.value, 10)
