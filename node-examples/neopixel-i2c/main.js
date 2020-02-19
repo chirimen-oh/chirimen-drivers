d2a9837fd22410d4f6014863c91d38ce62d2e01f
Masato Tomita <tomita13112@gmail.com>
2020-02-19 14:01:16 +0900

    Fix codes to camel case

--- node-examples/neopixel-i2c/main.js
+++ node-examples/neopixel-i2c/main.js
@@ -1,5 +1,5 @@
 const { requestI2CAccess } = require("node-web-i2c");
-const NEOPIXEL_I2C = require("@chirimen/neopixel-i2c");
+const NEOPIXEL = require("@chirimen/neopixel-i2c");
 const { promisify } = require("util");
 const sleep = promisify(setTimeout);
 
@@ -8,15 +8,15 @@ main();
 async function main() {
   const i2cAccess = await requestI2CAccess();
   const port = i2cAccess.ports.get(1);
-  const neopixel_i2c = new NEOPIXEL_I2C(port, 0x41);
-  await neopixel_i2c.init();
+  const neopixel = new NEOPIXEL(port, 0x41);
+  await neopixel.init();
 
   while (true) {
-    await neopixel_i2c.setGlobal(64, 0, 0); // Red
+    await neopixel.setGlobal(64, 0, 0); // Red
     await sleep(200);
-    await neopixel_i2c.setGlobal(0, 64, 0); // Green
+    await neopixel.setGlobal(0, 64, 0); // Green
     await sleep(200);
-    await neopixel_i2c.setGlobal(0, 0, 64); // Blue
+    await neopixel.setGlobal(0, 0, 64); // Blue
     await sleep(200);
   }
 }
