2bbc02c41daabe6b4d5f254d924a39134f36f6cc
rasp753 <21194634+rasp753@users.noreply.github.com>
2020-02-26 17:40:28 +0900

    fix way to connect MSB and LSB

--- packages/adt7410/adt7410.mjs
+++ packages/adt7410/adt7410.mjs
@@ -15,7 +15,7 @@ ADT7410.prototype = {
 
     const MSB = await this.i2cSlave.read8(0x00);
     const LSB = await this.i2cSlave.read8(0x01);
-    const binaryTemperature = ((MSB << 8) + LSB) >> 3;
+    const binaryTemperature = ((MSB << 8) | LSB) >> 3;
     const sign = MSB & 0x10;
     // Under 13bit resolution mode. (sign + 12bit)
     if (sign === 0) {
