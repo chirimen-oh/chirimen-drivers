8e56f4793c2e503296479faef8a2f74d471985ec
rasp753 <21194634+rasp753@users.noreply.github.com>
2020-02-26 17:20:21 +0900

    Fix how to judge whether the value is negative.
    Co-Authored-By: Kohei Watanabe <kou029w@gmail.com>

--- packages/adt7410/adt7410.mjs
+++ packages/adt7410/adt7410.mjs
@@ -16,7 +16,8 @@ ADT7410.prototype = {
     const MSB = await this.i2cSlave.read8(0x00);
     const LSB = await this.i2cSlave.read8(0x01);
     const rawData = ((MSB << 8) + LSB) >> 3;
-    if (MSB < 16) {
+    const sign = MSB & 0x10;
+    if (sign === 0) {
       // Positive value
       return rawData / 16.0;
     } else {
