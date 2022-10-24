d5869152d8f5fd5835790281460689bc00f9ea34
Satoru Takagi <satakagi@gmail.com>
2022-10-25 08:18:32 +0900

    Add HTU21D, and fix ccs811,bme680

---
+++ packages/bme680/rollup.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  input: "bme680.js",
+  output: {
+    file: "index.js",
+    format: "umd",
+    name: "BME680"
+  }
+};
---
+++ packages/ccs811/rollup.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  input: "ccs811.js",
+  output: {
+    file: "index.js",
+    format: "umd",
+    name: "CCS811"
+  }
+};
