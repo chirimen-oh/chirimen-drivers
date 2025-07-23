fc5437d6bf059d9a61157f3eb660946cbbb34aeb
akihiko.kigure <akihiko.kigure@gmail.com>
2025-07-23 23:09:36 +0900

    build: setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+export default [
+  {
+    files: ['packages/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // 必要に応じてルールを追加
+    },
+  },
+];
