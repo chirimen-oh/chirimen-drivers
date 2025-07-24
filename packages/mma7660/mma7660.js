0d3824b2bb9d4c4c3516f0daf29307a58fa66678
ms-inoue-dev <m.inoue.0802@gmail.com>
2025-07-24 15:40:40 +0900

    プルリクエストでの指摘部分の修正

--- packages/mma7660/mma7660.js
+++ packages/mma7660/mma7660.js
@@ -1,35 +1,37 @@
 // @ts-check
-// PCF8591 driver for CHIRIMEN
+// MMA7660 driver for CHIRIMEN
 // Based from https://github.com/Seeed-Studio/Accelerometer_MMA7660/blob/master/MMA7660.cpp
-// Programmed by Masahito inoue
+// Programmed by Masahito Inoue
 
 const MMA7660_MODE = 0x07;
 const MMA7660_STAND_BY = 0x00;
 const MMA7660_ACTIVE = 0x01;
 const MMA7660_SR = 0x08;      //sample rate register
 const AUTO_SLEEP_32 = 0X02;
+const ACCELERATION_DIVISOR = 21.00;
 
-class MMA7660{
-  constructor(i2cPort,slaveAddress){
+class MMA7660 {
+  constructor(i2cPort, slaveAddress) {
     this.i2cPort = i2cPort;
     this.i2cSlave = null;
     this.slaveAddress = slaveAddress;
   }
 
-  async init(){
+  async init() {
     this.i2cSlave = await this.i2cPort.open(this.slaveAddress);
     await this.i2cSlave.write8(MMA7660_MODE, MMA7660_STAND_BY);
     await this.i2cSlave.write8(MMA7660_SR, AUTO_SLEEP_32);
     await this.i2cSlave.write8(MMA7660_MODE, MMA7660_ACTIVE);
   }
 
-  async getXYZ(){
+  async getXYZ() {
     if (this.i2cSlave == null) {
       throw new Error("i2cSlave is not open yet.");
     }
 
     const XYZresult = new Int8Array(await this.i2cSlave.readBytes(3));
 
+    // Calculations are performed by bit shifting to remove noise
     const XYZdata = {
       "X" : (XYZresult[0] << 2) / 4,
       "Y" : (XYZresult[1] << 2) / 4,
@@ -39,7 +41,7 @@ class MMA7660{
     return XYZdata;
   }
 
-  async getAcceleration(){
+  async getAcceleration() {
     if (this.i2cSlave == null) {
       throw new Error("i2cSlave is not open yet.");
     }
@@ -47,12 +49,12 @@ class MMA7660{
     const XYZdata = await this.getXYZ();
     
     const AccelerationData = {
-      "X" : XYZdata.X / 21.00,
-      "Y" : XYZdata.Y / 21.00,
-      "Z" : XYZdata.Z / 21.00,
+      "X" : XYZdata.X / ACCELERATION_DIVISOR,
+      "Y" : XYZdata.Y / ACCELERATION_DIVISOR,
+      "Z" : XYZdata.Z / ACCELERATION_DIVISOR,
     };
 
-    return AccelerationData
+    return AccelerationData;
   }
 }
 
