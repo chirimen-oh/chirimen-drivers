7af01110c84437956e6a25daf910c73d7fb05a92
Jasmine <jasmineortiz5425@gmail.com>
2026-08-21 11:25:39 +0900

    refactor: remove undocumented setMode(), unused CONFIG_REGISTER constant, and fix JSDoc typo

--- packages/tmp117/index.js
+++ packages/tmp117/index.js
@@ -4,7 +4,6 @@
 // Based from https://www.ti.com/lit/ds/symlink/tmp117.pdf
 
 const TEMP_RESULT_REGISTER = 0x00;
-const CONFIG_REGISTER = 0x01;
 const DEVICE_ID_REGISTER = 0x0f;
 const EXPECTED_DEVICE_ID_MASK = 0x0fff; // lower 12 bits are DID; upper 4 bits are silicon revision
 const EXPECTED_DEVICE_ID = 0x0117;
@@ -14,23 +13,16 @@ const GENERAL_CALL_ADDRESS = 0x00;
 const GENERAL_CALL_RESET_DATA = 0x06;
 const RESET_WAIT_MS = 20;
 
-const MODE_CONTINUOUS = 0b00;
-const MODE_SHUTDOWN = 0b01;
-const MODE_ONESHOT = 0b11;
-const MODE_MASK = 0b0000110000000000;
-const MODE_SHIFT = 10;
-
 class TMP117 {
   /**
    * @constructor
    * @param {import('node-web-i2c').I2CPort} i2cPort I2C port instance
-   * @param {number} [slaveAddres]s I2C slave address (default: 0x48)
+   * @param {number} [slaveAddress] I2C slave address (default: 0x48)
    */
   constructor(i2cPort, slaveAddress = 0x48) {
     this.i2cPort = i2cPort;
     this.i2cSlave = null;
     this.slaveAddress = slaveAddress;
-    this.mode = MODE_CONTINUOUS;
   }
 
   async init() {
@@ -59,17 +51,11 @@ class TMP117 {
    * TI sensors) - not just this TMP117 instance. Avoid calling this
    * if other General-Call-capable devices are sharing the bus and
    * should not be reset.
-   *
-   * Resets the hardware CONFIGURATION register back to its factory
-   * default (continuous conversion mode), so this.mode is reset to
-   * MODE_CONTINUOUS here to keep it in sync with the actual hardware
-   * state.
    */
   async reset() {
     const generalCallSlave = await this.i2cPort.open(GENERAL_CALL_ADDRESS);
     await generalCallSlave.writeByte(GENERAL_CALL_RESET_DATA);
     await this.#wait(RESET_WAIT_MS);
-    this.mode = MODE_CONTINUOUS;
   }
 
   #wait(ms) {
@@ -86,32 +72,6 @@ class TMP117 {
     }
     const rawValue = await this.i2cSlave.read16(register);
     return this.#swapBytes(rawValue);
-  }
-
-  async #writeRegister16(register, value) {
-    if (this.i2cSlave == null) {
-      throw new Error("i2cSlave is not open yet.");
-    }
-    await this.i2cSlave.write16(register, this.#swapBytes(value));
-  }
-
-  /**
-   * @param {number} mode
-   */
-  async setMode(mode) {
-    if (
-      mode !== MODE_CONTINUOUS &&
-      mode !== MODE_SHUTDOWN &&
-      mode !== MODE_ONESHOT
-    ) {
-      throw new Error("TMP117.setMode() invalid mode!");
-    }
-    this.mode = mode;
-    let config = await this.#readRegister16(CONFIG_REGISTER);
-    config = config & ~MODE_MASK;
-    config |= mode << MODE_SHIFT;
-    await this.#writeRegister16(CONFIG_REGISTER, config);
-    return mode;
   }
 
   async read() {
